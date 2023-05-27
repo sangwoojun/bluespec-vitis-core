d7e699d18aca48ce4bffc39b4913f7154f9a8978
SeMinLim <seminl1@ics.uci.edu>
2023-05-27 15:38:29 -0700

    modified top wrapper dcp file's name

--- scripts/report.tcl
+++ scripts/report.tcl
@@ -1,7 +1,6 @@
 set outputdir ./reports
 file mkdir $outputdir
-#open_checkpoint _x/link/vivado/prj/prj.runs/impl_1/pfm_top_wrapper_routed.dcp
-open_checkpoint ./_x/link/vivado/vpl/prj/prj.runs/impl_1/pfm_top_wrapper_routed.dcp
+open_checkpoint ./_x/link/vivado/vpl/prj/prj.runs/impl_1/level0_wrapper_routed.dcp
 
 report_timing_summary -file $outputdir/timing_summary.rpt
 report_utilization -file $outputdir/utilization.rpt
