93b0a17cf3d158c183dc255f2ee9c3746bc911b7
SeMinLim <seminl1@ics.uci.edu>
2024-11-16 23:25:23 -0800

    modified Makefile to use bluelib

--- hw/bancroft_kernel/c-model/kernel.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-extern "C" {
-	void mkKernelTop(int size, int* mem, int* file) { // Size in integer
-		for ( int i = 0; i < size; i++ ) mem[i] = i;
-	}
-}
--- sw/bancroft_host/ColumnSorter.cpp
+++
@@ -1,1 +0,0 @@
-#include "ColumnSorter.h"
